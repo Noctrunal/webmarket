d37515f2337e9d874d0cec4cb5121a359861ccc3
Nocturnal <mister.cheban@gmail.com>
2016-05-26 01:48:36 +0400

    Add Bootstrap template for admin page

--- src/main/java/com/webmarket/application/controller/web/RootController.java
+++ src/main/java/com/webmarket/application/controller/web/RootController.java
@@ -9,9 +9,14 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 public class RootController extends AbstractProductsController {
-    @RequestMapping(value = "/", method = GET)
+    @RequestMapping(value = "/products", method = GET)
     public String root(Model model) {
         model.addAttribute("products", super.getAll());
         return "productsList";
+    }
+
+    @RequestMapping(value = {"/", "/login"}, method = GET)
+    public String login() {
+        return "login";
     }
 }
