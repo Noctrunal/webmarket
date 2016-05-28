40d3e4647bf0a42f0647597a9a91564a7975c620
Nocturnal <mister.cheban@gmail.com>
2016-05-28 18:19:03 +0400

    Add export buttons, and refactor view

--- src/main/resources/static/javascript/productsDatatables.js
+++ src/main/resources/static/javascript/productsDatatables.js
@@ -27,7 +27,13 @@ $(function () {
                 'data': 'description'
             },
             {
-                'data': 'price'
+                'orderable': false,
+                'defaultContent': '',
+                'render': function (data, type, row) {
+                    if (type == 'display') {
+                        return '$' + row.price;
+                    }
+                }
             },
             {
                 'data': 'amount'
@@ -57,6 +63,52 @@ $(function () {
                 'render': renderDeleteButton
             }
         ],
+        dom: 'Bfrtip',
+        buttons:
+            [
+            {
+                extend: 'collection',
+                text: 'Export <span class="glyphicon glyphicon-export"></span>',
+                buttons:
+                [
+                    {
+                        extend: 'copyHtml5',
+                        text: 'Copy <i class="fa fa-files-o"></i>',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4, 5, 6, 7]
+                        }
+                    },
+                    {
+                        extend: 'csvHtml5',
+                        text: 'CSV <i class="fa fa-file-text-o"></i>',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4, 5, 6, 7]
+                        }
+                    },
+                    {
+                        extend: 'excelHtml5',
+                        text: 'Excel <i class="fa fa-file-excel-o"></i>',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4, 5, 6, 7]
+                        }
+                    },
+                    {
+                        extend: 'pdfHtml5',
+                        text: 'PDF <i class="fa fa-file-pdf-o"></i>',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4, 5, 6, 7]
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        text: 'Print <span class="glyphicon glyphicon-print"></span>',
+                        exportOptions: {
+                            columns: [0, 1, 2, 3, 4, 5, 6, 7]
+                        }
+                    }
+                ]
+            }
+            ],
         'order': [
             [
                 0,
