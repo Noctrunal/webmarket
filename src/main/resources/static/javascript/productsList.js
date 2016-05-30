98876caf4389cac0c349d131c63a9010d5d327c2
Nocturnal <mister.cheban@gmail.com>
2016-05-31 01:15:28 +0400

    Refactor list/grid system

--- src/main/resources/static/javascript/productsList.js
+++ src/main/resources/static/javascript/productsList.js
@@ -1,15 +1,11 @@
-//noinspection JSUnresolvedFunction
 $(document).ready(function () {
-    //noinspection JSUnresolvedFunction
     $('#list').click(function (event) {
-        event.preventDefault();//noinspection JSJQueryEfficiency,JSUnresolvedFunction
+        event.preventDefault();
         $('#products .item').addClass('list-group-item');
     });
-    //noinspection JSUnresolvedFunction
     $('#grid').click(function (event) {
-        event.preventDefault();//noinspection JSJQueryEfficiency,JSUnresolvedFunction
+        event.preventDefault();
         $('#products .item').removeClass('list-group-item');
-        //noinspection JSJQueryEfficiency,JSUnresolvedFunction
         $('#products .item').addClass('grid-group-item');
     });
 });
